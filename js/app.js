c50fe966315e69f2111eef51a330417db6de69eb
singleMaltMatt <mattmatte93@gmail.com>
2022-03-30 20:53:56 +0200

    scripting frog movements.

--- js/app.js
+++ js/app.js
@@ -2,29 +2,120 @@ const timeLeftDisplay = document.querySelector('#time-left');
 const resultDisplay = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
-console.log(squares)
 
-let currentIndex = 76;
+let currentIndex = 75;
+const width = 9;
 
 //MOVE FROG
-function moveFrog(e) {
+function moveFrog(e) { 
+    console.log(squares)
 
     switch (e.key) {
         case 'ArrowLeft':
-
+            currentIndex -= 1 
             break
         case 'ArrowRight':
-
+            currentIndex += 1
             break
         case 'ArrowUp':
-
+            currentIndex -= width
             break
         case 'ArrowDown':
-
+            currentIndex += width
             break
     }
 
     squares[currentIndex].classList.add('frog')
-
 }
 document.addEventListener('keyup', moveFrog)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     switch (e.key) {
+//         case 'ArrowLeft':
+//                 console.log('move left')
+//             break
+//         case 'ArrowRight':
+
+//             break
+//         case 'ArrowUp':
+
+//             break
+//         case 'ArrowDown':
+
+//             break
+//     }
+//     squares[currentIndex].classList.add('frog')
+
+// }
+// document.addEventListener('keyup', moveFrog)
